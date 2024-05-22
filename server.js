b1b788e01286e79fc107a1e2f754443bee5699c7
Matt Sullivan <mattsully01@gmail.com>
2024-05-22 08:50:17 -0600

    Adding contact form func

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser')
+// require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 // Import route files
@@ -12,10 +15,12 @@ const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use('/', htmlRoutes)
 
+
 // middleware 
 app.use((req, res, next) => {
     res.status(404).send("Sorry can't find that!");
 });
+
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
